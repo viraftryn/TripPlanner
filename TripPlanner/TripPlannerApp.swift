62923c4f897b45bc13b1208f7e4314d2fed5182b
Kristanto Sean <kristantosean@gmail.com>
2026-09-05 16:47:56 +0700

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Kristanto Sean on 2026-09-05.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
